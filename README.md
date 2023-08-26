6f8862df2a82ddee7c82948a6f348467098d2ec6
Arfin Foysal <arfinfoysalbd@gmail.com>
2023-08-27 00:24:47 +0600

    my fast npm project

---
+++ README.md
@@ -0,0 +1,17 @@
+Install component-generator CLI
+
+Open Your Terminal in Your Current Project Or Directory. 
+
+$ foysal-g "directoryName componentName"
+
+Or
+
+$ foysal-g "componentName"
+
+
+Generate app directory 
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 #!/usr/bin/env node
 const fs = require("fs");
-if (fs.existsSync("src")) {
- 
-} else {
-  fs.mkdir("src", (err) => {
+if (process.argv.length === 4) {
+  const dirName = process.argv[2];
+    if (!fs.existsSync(dirName)) {
+        fs.mkdir(dirName, (err) => {
+            if (err) throw err;
+            console.log(`${dirName} created successfully😊.
+            `);
+        }
+        );
+    }
+
+    const fileName = process.argv[3].charAt(0).toUpperCase() + process.argv[3].slice(1) ||
+    "hello-world.jsx";
+  const fileContent = `
+    import React from 'react'
+    
+    export const ${fileName} = () => {
+        return (
+          <div>${fileName}</div>
+        )
+      }`;
+    
+    //if file already exists
+    if (fs.existsSync(`${dirName}/${fileName}.jsx`)) {
+        console.log(`${fileName}.jsx already exists in ${dirName}😐.
+        `);
+        return;
+    }
+
+  fs.writeFile(`${dirName}/${fileName}.jsx`, fileContent, (err) => {
     if (err) throw err;
-    console.log(` src directory created 😊`);
+    console.log(`${fileName}.jsx created successfully😊.
+          `);
   });
-}
-
-if (fs.existsSync("src/app")) {
-
+    
+    
 } else {
-  fs.mkdir("src/app", (err) => {
-    if (err) throw err;
-    console.log(`app directory created 😊`);
-  });
-}
-
-const fileName =
-  process.argv[2].charAt(0).toUpperCase() + process.argv[2].slice(1) ||
-  "hello-world.js";
-
-const fileContent = `
+  const fileName =
+    process.argv[2].charAt(0).toUpperCase() + process.argv[2].slice(1) ||
+    "hello-world.jsx";
+  const fileContent = `
 import React from 'react'
 
 export const ${fileName} = () => {
@@ -30,10 +48,17 @@ export const ${fileName} = () => {
       <div>${fileName}</div>
     )
   }`;
+    
+    //if file already exists
+    if (fs.existsSync(`${fileName}.jsx`)) {
+        console.log(`${fileName}.jsx already exists😐.
+    `);
+        return;
+    }
 
-fs.writeFile(`src/app/${fileName}.jsx`, fileContent, (err) => {
-  if (err) throw err;
-  console.log(`${fileName}.jsx created successfully😊.
-  
+  fs.writeFile(`${fileName}.jsx`, fileContent, (err) => {
+    if (err) throw err;
+    console.log(`${fileName}.jsx created successfully😊.
   `);
-});
+  });
+}
